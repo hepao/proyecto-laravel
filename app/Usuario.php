8bdf2af8e0822a6123d63cc8e4aa382bf762ebfe
hepao <50034742+hepao@users.noreply.github.com>
2019-06-20 20:53:43 -0500

    Avancee

---
+++ app/Usuario.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Usuario extends Model
+{
+    protected  $table="usuario";
+    protected  $primaryKey="idusuario";
+    protected  $fillable=["primer_nombre","segundo_nombre","primer_apellido","segundo_apellido","documento","correo_personal","corre_institucional","edad"];
+}

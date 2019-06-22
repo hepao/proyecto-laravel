40628b8243149f97740df3e923e02799e0251c63
hepao <50034742+hepao@users.noreply.github.com>
2019-06-21 20:44:48 -0500

    nuevo cc

---
+++ app/Adminsitrador.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Adminsitrador extends Model
+{
+    /*protected  $table="usuario";
+    protected  $primaryKey="idusuario";
+    protected  $fillable=["primer_nombre","segundo_nombre","primer_apellido","segundo_apellido","documento","correo_personal","corre_institucional","edad"];
+    */
+}
